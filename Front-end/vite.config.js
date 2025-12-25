93154296c696645dc2a40a5ac4918975f32f1549
Chen YanHao <odallby@outlook.com>
2025-12-25 18:59:19 +0800

    test
    test

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8082',
         changeOrigin: true
         // 移除rewrite配置，保留/api前缀
       }
